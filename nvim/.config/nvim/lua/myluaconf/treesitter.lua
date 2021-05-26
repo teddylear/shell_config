265a2048e520671753656bb16268d4a7e8ee9719
teddylear <teddylear@gmail.com>
2021-05-25 22:40:04 -0400

    Adding ensure installed treesitter config languages

--- nvim/.config/nvim/lua/myluaconf/treesitter.lua
+++ nvim/.config/nvim/lua/myluaconf/treesitter.lua
@@ -1,5 +1,14 @@
 require'nvim-treesitter.configs'.setup {
+    ensure_installed = {
+        "python",
+        "go",
+        "lua",
+        "yaml",
+        "json",
+        "bash",
+        "dockerfile",
+    },
     highlight = {
         enable = true
-    }
+    },
 }

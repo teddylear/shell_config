aaeed5a9a32378602b6840557e892596669299de
teddylear <teddylear@gmail.com>
2021-05-30 23:27:48 -0400

    Adding ubuntu steps to install script and TODO comments for more work

--- install/mac_install.sh
+++ install/mac_install.sh
@@ -4,6 +4,8 @@
 brew tap clementtsang/bottom
 brew install bottom
 
+# TODO add installing docker
+
 # Installing lazydocker
 brew install jesseduffield/lazydocker/lazydocker
 
---
+++ install/ubuntu_install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/zsh
+
+# Installing bottom
+curl -LO https://github.com/ClementTsang/bottom/releases/download/0.6.1/bottom_0.6.1_amd64.deb
+# This version should change or live in a configuration file
+# somewhere between scripts
+sudo dpkg -i bottom_0.6.1_amd64.deb
+
+# Installing docker from docks: https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+# Having it run on startup
+sudo systemctl enable docker
+
+# Adding current user to docker group
+sudo usermod -aG docker $USER
+newgrp docker
+
+# Installing lazydocker via docs
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+
+# TODO add ripgrep

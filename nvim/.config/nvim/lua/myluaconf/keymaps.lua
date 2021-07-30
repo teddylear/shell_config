6498019f692247e244af2fef0a2ca1ca8fb32c1d
teddylear <teddylear@gmail.com>
2021-07-30 19:30:41 -0400

    Refactoring keymaps to be under 80 char limit

--- nvim/.config/nvim/lua/myluaconf/keymaps.lua
+++ nvim/.config/nvim/lua/myluaconf/keymaps.lua
@@ -14,35 +14,115 @@ local function init()
     map('n', '<leader>co', '<CMD>Copen<CR>', options)
 
     -- lsp configuration configuration
-    map("n", "<leader>gd", "<CMD>lua vim.lsp.buf.definition()<CR>", options)
+    map(
+        "n",
+        "<leader>gd",
+        "<CMD>lua vim.lsp.buf.definition()<CR>",
+        options
+    )
     -- TODO: have to find out how this works, don't have an example of this
-    -- map('n', '<leader>gi', '<CMD>lua vim.lsp.buf.implementation()<CR>', options)
-    map('n', '<leader>gfh', '<CMD>lua vim.lsp.buf.signature_help()<CR>', options)
-    map('n', '<leader>gr', '<CMD>lua vim.lsp.buf.references()<CR>', options)
+    -- map(
+        -- 'n',
+        -- '<leader>gi',
+        -- '<CMD>lua vim.lsp.buf.implementation()<CR>',
+        -- options
+    -- )
+    map(
+        'n',
+        '<leader>gfh',
+        '<CMD>lua vim.lsp.buf.signature_help()<CR>',
+        options
+    )
+    map(
+        'n',
+        '<leader>gr',
+        '<CMD>lua vim.lsp.buf.references()<CR>',
+        options
+    )
 
     -- Telescope keymaps
-    map('n', '<leader>pw', '<CMD>lua require("telescope.builtin").live_grep()<CR>', options)
-    map('n', '<leader>pb', '<CMD>lua require("telescope.builtin").buffers()<CR>', options)
-    map('n', '<leader>vh', '<CMD>lua require("telescope.builtin").help_tags()<CR>', options)
-    map('n', '<C-p>', '<CMD>lua require("telescope.builtin").git_files()<CR>', options)
-    map('n', '<leader>pf', '<CMD>lua require("telescope.builtin").find_files()<CR>', options)
+    map(
+        'n',
+        '<leader>pw',
+        '<CMD>lua require("telescope.builtin").live_grep()<CR>',
+        options
+    )
+    map(
+        'n',
+        '<leader>pb',
+        '<CMD>lua require("telescope.builtin").buffers()<CR>',
+        options
+    )
+    map(
+        'n',
+        '<leader>vh',
+        '<CMD>lua require("telescope.builtin").help_tags()<CR>',
+        options
+    )
+    map(
+        'n',
+        '<C-p>',
+        '<CMD>lua require("telescope.builtin").git_files()<CR>',
+        options
+    )
+    map(
+        'n',
+        '<leader>pf',
+        '<CMD>lua require("telescope.builtin").find_files()<CR>',
+        options
+    )
 
     -- Floaterm commands
-    map('n', '<leader>bt', '<CMD>FloatermNew --autoclose=1 --height=0.9 --width=0.9 btm<CR>', options)
-    map('n', '<leader>ld', '<CMD>FloatermNew --autoclose=1 --height=0.9 --width=0.9 lazydocker<CR>', options)
-    map('n', '<leader>tt', '<CMD>FloatermNew --autoclose=1 --height=0.9 --width=0.9<CR>', options)
+    map(
+        'n',
+        '<leader>bt',
+        '<CMD>FloatermNew --autoclose=1 --height=0.9 --width=0.9 btm<CR>',
+        options
+    )
+    map(
+        'n',
+        '<leader>ld',
+        '<CMD>FloatermNew --autoclose=1 --height=0.9 --width=0.9 lazydocker<CR>',
+        options
+    )
+    map(
+        'n',
+        '<leader>tt',
+        '<CMD>FloatermNew --autoclose=1 --height=0.9 --width=0.9<CR>',
+        options
+    )
 
     -- TODO: Have to experiment more with lspsaga commands and configuration
-    map('n', '<leader>re', '<CMD>lua require("lspsaga.rename").rename()<CR>', options)
+    map(
+        'n',
+        '<leader>re',
+        '<CMD>lua require("lspsaga.rename").rename()<CR>',
+        options
+    )
 
-    map('n', '<leader>nn', '<CMD>lua require("myluaconf.functions").NewNote()<CR>', options)
+    map(
+        'n',
+        '<leader>nn',
+        '<CMD>lua require("myluaconf.functions").NewNote()<CR>',
+        options
+    )
 
     -- TODO: add these commands to cheatsheet
     map('t', 'jk', '<C-\\><C-n>', options)
 
     -- harpoon commands
-    map('n','<leader>a', '<CMD>lua require("harpoon.mark").add_file()<CR>', options)
-    map('n','<C-e>', '<CMD>lua require("harpoon.ui").toggle_quick_menu()<CR>', options)
+    map(
+        'n',
+        '<leader>a',
+        '<CMD>lua require("harpoon.mark").add_file()<CR>',
+        options
+    )
+    map(
+        'n',
+        '<C-e>',
+        '<CMD>lua require("harpoon.ui").toggle_quick_menu()<CR>',
+        options
+    )
     map('n','<C-i>', '<CMD>lua require("harpoon.ui").nav_file(1)<CR>', options)
     map('n','<C-t>', '<CMD>lua require("harpoon.ui").nav_file(2)<CR>', options)
     map('n','<C-n>', '<CMD>lua require("harpoon.ui").nav_file(3)<CR>', options)

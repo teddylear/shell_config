bb8812ef66920cc3b27c038e0b2d7c0b0b46dd8b
teddylear <teddylear@gmail.com>
2021-12-03 21:52:41 -0500

    Adding starting journal to tmux script

--- tmux-session.sh
+++ tmux-session.sh
@@ -3,6 +3,7 @@
 SESSIONNAME="personal"
 tmux new-session -s $SESSIONNAME -c "$HOME/Documents/journal" -d
 tmux rename-window -t 0 "JOURNAL"
+tmux send-key -t 0 vim Space . Enter\
 
 tmux new-window -c "$HOME/shell_config" -n "SHELL_CONFIG" -t $SESSIONNAME
 tmux move-window -s 1 -t 9

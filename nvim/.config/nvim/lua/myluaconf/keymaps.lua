6db5eb0bda94e6b973da70cca1d88feac3a4291e
teddylear <teddylear@gmail.com>
2021-12-31 16:14:20 -0500

    adding prompt variable to config

--- nvim/.config/nvim/lua/myluaconf/keymaps.lua
+++ nvim/.config/nvim/lua/myluaconf/keymaps.lua
@@ -7,7 +7,10 @@ local function init()
     require('refactoring').setup({
         prompt_func_return_type = {
             go = true
-        }
+        },
+        prompt_func_param_type = {
+            go = true
+        },
     })
 
     -- TODO: automate how these are loaded to cheatsheet

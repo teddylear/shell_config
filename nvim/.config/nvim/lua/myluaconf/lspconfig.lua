22c5f75c43057d838546568bef678884ecd929ce
teddylear <teddylear@gmail.com>
2022-02-26 23:21:29 -0500

    Fixing lua lspserver to be better

--- nvim/.config/nvim/lua/myluaconf/lspconfig.lua
+++ nvim/.config/nvim/lua/myluaconf/lspconfig.lua
@@ -69,6 +69,9 @@ require("lspconfig").sumneko_lua.setup({
     capabilities = capabilities,
     cmd = require("lspcontainers").command("sumneko_lua"),
     settings = lua_settings,
+    on_new_config = function(new_config, new_root_dir)
+        new_config.cmd = require'lspcontainers'.command("sumneko_lua", { root_dir = new_root_dir })
+      end
 })
 
 -- Required for refactoring plugin dev

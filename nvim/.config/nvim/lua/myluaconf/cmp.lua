491ccff76cb90dfd73494071ea6043c651f14706
teddylear <teddylear@gmail.com>
2022-03-10 19:44:15 -0500

    feat: Adding support for tabnine

--- nvim/.config/nvim/lua/myluaconf/cmp.lua
+++ nvim/.config/nvim/lua/myluaconf/cmp.lua
@@ -11,10 +11,38 @@ local has_words_before = function()
             == nil
 end
 
+local source_mapping = {
+	buffer = "[Buffer]",
+	nvim_lsp = "[LSP]",
+	nvim_lua = "[Lua]",
+	cmp_tabnine = "[TN]",
+	path = "[Path]",
+}
+
+local tabnine = require('cmp_tabnine.config')
+tabnine:setup({
+	max_lines = 1000;
+	max_num_results = 20;
+	sort = true;
+	run_on_every_keystroke = true;
+	snippet_placeholder = '..';
+})
+
 cmp.setup({
     formatting = {
-        format = lspkind.cmp_format({ with_text = false, maxwidth = 50 }),
-    },
+		format = function(entry, vim_item)
+			vim_item.kind = lspkind.presets.default[vim_item.kind]
+			local menu = source_mapping[entry.source.name]
+			if entry.source.name == 'cmp_tabnine' then
+				if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
+					menu = entry.completion_item.data.detail .. ' ' .. menu
+				end
+				vim_item.kind = ''
+			end
+			vim_item.menu = menu
+			return vim_item
+		end
+	},
     snippet = {
         expand = function(args)
             ls.lsp_expand(args.body)
@@ -43,6 +71,7 @@ cmp.setup({
         { name = "buffer" },
         { name = "treesitter" },
         { name = "luasnip" },
+        -- { name = 'cmp_tabnine' },
     },
 })
 

b2e3b3701c0eb93c8b0c9044a37e75f7843fb888
teddylear <teddylear@gmail.com>
2022-03-15 17:13:24 -0400

    feat: adding reload command to test things much easier

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,20 @@
-require("myluaconf")
+require("myluaconf.packer").init()
+require("myluaconf.todo_comments")
+require("myluaconf.telescope")
+require("myluaconf.lualine")
+require("myluaconf.luasnip")
+require("myluaconf.cmp")
+require("myluaconf.treesitter")
+require("myluaconf.lspconfig")
+require("myluaconf.sets")
+require("myluaconf.functions")
+require("myluaconf.keymaps").init()
+
+if pcall(require, 'plenary') then
+  RELOAD = require('plenary.reload').reload_module
+
+  R = function(name)
+    RELOAD(name)
+    return require(name)
+  end
+end

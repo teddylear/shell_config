15f1aad682d55e51eda7195e92c5218b8fdf16cb
teddylear <teddylear@gmail.com>
2022-05-18 23:19:47 -0400

    chore: cleanup lualine

--- nvim/.config/nvim/lua/myluaconf/lualine.lua
+++ nvim/.config/nvim/lua/myluaconf/lualine.lua
@@ -1,14 +1,10 @@
 require("lualine").setup({
     options = {
         theme = "nord",
-        separator = "|",
         icons_enabled = true,
-        extensions = { "fzf", "fugitive" },
         -- This sets relative path
         path = 1,
         globalstatus = true,
     },
-    lualine_c = {
-        { "filename" },
-    },
+    extensions = { "fzf", "fugitive" },
 })

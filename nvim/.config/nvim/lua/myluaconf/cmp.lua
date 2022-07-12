de3746fdaca258947e9dc9ab591ce7cb97afa976
teddylear <teddylear@gmail.com>
2022-07-11 22:14:19 -0400

    chore: cleanup nvim-cmp settings to be cleaner

--- nvim/.config/nvim/lua/myluaconf/cmp.lua
+++ nvim/.config/nvim/lua/myluaconf/cmp.lua
@@ -1,15 +1,7 @@
-local cmp = require("cmp")
 local lspkind = require("lspkind")
+lspkind.init()
+local cmp = require("cmp")
 local ls = require("luasnip")
-
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0
-        and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-                :sub(col, col)
-                :match("%s")
-            == nil
-end
 
 -- local tabnine = require("cmp_tabnine.config")
 -- tabnine:setup({
@@ -20,24 +12,8 @@ end
 -- snippet_placeholder = "..",
 -- })
 --
-local feedkey = function(key, mode)
-    vim.api.nvim_feedkeys(
-        vim.api.nvim_replace_termcodes(key, true, true, true),
-        mode,
-        true
-    )
-end
 
 cmp.setup({
-    formatting = {
-        format = lspkind.cmp_format({
-            mode = "symbol_text",
-            maxwidth = 50,
-            before = function(entry, vim_item)
-                return vim_item
-            end,
-        }),
-    },
     snippet = {
         expand = function(args)
             ls.lsp_expand(args.body)
@@ -50,22 +26,8 @@ cmp.setup({
         ["<C-e>"] = cmp.mapping.close(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<tab>"] = cmp.config.disable,
-        ["<C-n>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif vim.fn["vsnip#available"](1) == 1 then
-                feedkey("<Plug>(vsnip-expand-or-jump)", "")
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-            end
-        end, { "i", "s" }),
-        ["<C-p>"] = cmp.mapping(function()
-            if cmp.visible() then
-                cmp.select_prev_item()
-            end
-        end, { "i", "s" }),
+        ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+        ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
     },
     sources = {
         { name = "nvim_lua" },

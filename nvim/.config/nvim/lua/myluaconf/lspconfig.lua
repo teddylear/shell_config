8931b45d5391a94754f6cc1b7d5651412c831aea
teddylear <teddylear@protonmail.com>
2022-08-06 14:02:34 -0400

    feat: Not having lsp publish diagnostics in insert mode

--- nvim/.config/nvim/lua/myluaconf/lspconfig.lua
+++ nvim/.config/nvim/lua/myluaconf/lspconfig.lua
@@ -120,4 +120,10 @@ require("lspconfig").rust_analyzer.setup({
     -- },
 })
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    update_in_insert = false,
+  }
+)
+
 require("fidget").setup({})

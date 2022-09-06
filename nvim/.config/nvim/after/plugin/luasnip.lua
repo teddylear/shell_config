d037a177e8bfedad2ef7ba1530c2f01a1394b5b4
teddylear <teddylear@gmail.com>
2022-09-05 20:36:02 -0400

    chore(nvim): Added more luasnips for terraform. Added resource, data resource, module

--- nvim/.config/nvim/after/plugin/luasnip.lua
+++ nvim/.config/nvim/after/plugin/luasnip.lua
@@ -35,6 +35,9 @@ ls.add_snippets("terraform", {
     ls.parser.parse_snippet("vs", 'variable "$0" {\n  type = string\n}'),
     ls.parser.parse_snippet("vn", 'variable "$0" {\n  type = number\n}'),
     ls.parser.parse_snippet("vb", 'variable "$0" {\n  type = bool\n}'),
+    ls.parser.parse_snippet("rs", 'resource "$1" "$2" {\n\t$0\n}'),
+    ls.parser.parse_snippet("ds", 'data "$1" "$2" {\n\t$0\n}'),
+    ls.parser.parse_snippet("md", 'module "$1" {\n\tsource = "$0"\n}'),
 })
 
 -- TODO:Can I do something with adding "fmt" to top of file if not present?

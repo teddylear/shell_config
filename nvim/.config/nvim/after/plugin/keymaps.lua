a825c8f469ac7781b60b203c357e94acc0aadc66
teddylear <teddylear@gmail.com>
2022-09-07 18:25:07 -0400

    chore(nvim): Move non plugin keymaps to after/plugin folder

---
+++ nvim/.config/nvim/after/plugin/keymaps.lua
@@ -0,0 +1,51 @@
+local map = vim.api.nvim_set_keymap
+
+map("n", "<leader>co", "<CMD>copen<CR>", {
+    noremap = true,
+    desc = "Open quick fix list",
+})
+
+map("t", "jk", "<C-\\><C-n>", {
+    noremap = true,
+    desc = "Terminal mode back to normal mode",
+})
+
+-- center When going next or previous in search, or when merging with
+-- previous line
+map(
+    "n",
+    "n",
+    "nzzzv",
+    { noremap = true, desc = "Center when going next in search" }
+)
+map(
+    "n",
+    "N",
+    "Nzzzv",
+    { noremap = true, desc = "Center when going previous in search" }
+)
+map(
+    "n",
+    "J",
+    "mzJ`z",
+    { noremap = true, desc = "Center when merging pervious line" }
+)
+
+-- Setting undo breakpoints
+map("i", ",", ",<c-g>u", { noremap = true, desc = "Set undo breakpoint" })
+map("i", ".", ".<c-g>u", { noremap = true, desc = "Set undo breakpoint" })
+map("i", "!", "!<c-g>u", { noremap = true, desc = "Set undo breakpoint" })
+map("i", "?", "?<c-g>u", { noremap = true, desc = "Set undo breakpoint" })
+
+-- Setting Prime's keymaps for moving visual block up & down
+map("v", "J", ":m '>+1<CR>gv=gv", {
+    noremap = true,
+    silent = true,
+    desc = "In Visual mode move block down",
+})
+
+map("v", "K", ":m '<-2<CR>gv=gv", {
+    noremap = true,
+    silent = true,
+    desc = "In Visual mode move block up",
+})
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,3 @@
 require("myluaconf.packer_setup").init()
 require("myluaconf.sets")
 require("myluaconf.functions").init()
-require("myluaconf.keymaps").init()

8208ab117581e62c93fa0429be302fdeb0287338
teddylear <teddylear@gmail.com>
2022-09-29 17:14:47 -0400

    fix(nvim): But in temp autocmd for resolving lua treesitter highlight issue

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -3,6 +3,7 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     command = "set filetype=cue",
 })
 
+
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 parser_config.cue = {
     install_info = {
@@ -38,3 +39,10 @@ require("nvim-treesitter.configs").setup({
 })
 
 require("treesitter-context").setup()
+
+-- HACK: Temp fix for lua while github issue is open
+-- https://github.com/nvim-treesitter/nvim-treesitter/issues/3538
+vim.api.nvim_create_autocmd({ "BufEnter"}, {
+    pattern = { "*.lua" },
+    command = "TSBufToggle highlight",
+})

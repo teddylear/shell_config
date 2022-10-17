f0abc005a299492574eafca66d0fa73715f0db31
teddylear <teddylear@gmail.com>
2022-10-17 19:19:38 -0400

    feat(nvim): add getting pipenv venv when setting up pyright lsp

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -43,9 +43,32 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 -- cmd = require("lspcontainers").command("pylsp"),
 -- })
 
+-- From below thread on this issue
+-- https://github.com/neovim/nvim-lspconfig/issues/500
+local path = util.path
+local function get_python_path(workspace)
+    -- Use activated virtualenv.
+    if vim.env.VIRTUAL_ENV then
+        return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+    end
+
+    -- Find and use virtualenv from pipenv in workspace directory.
+    local match = vim.fn.glob(path.join(workspace, 'Pipfile'))
+    if match ~= '' then
+        local venv = vim.fn.trim(vim.fn.system('PIPENV_PIPFILE=' .. match .. ' pipenv --venv'))
+        return path.join(venv, 'bin', 'python')
+    end
+
+    -- Fallback to system Python.
+    return vim.fn.exepath('python3') or vim.fn.exepath('python') or 'python'
+end
+
 require("lspconfig").pyright.setup({
     before_init = function(params)
         params.processId = vim.NIL
+    end,
+    on_init = function(client)
+        client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
     end,
     cmd = require("lspcontainers").command("pyright"),
     root_dir = util.root_pattern(".git", vim.fn.getcwd()),

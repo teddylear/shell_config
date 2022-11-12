1360473925af93da707a2fca41a7e25722363fab
teddylear <teddylear@gmail.com>
2022-11-12 11:54:00 -0500

    feat(nvim): Add function to add fmt package if not present for golang luasnips that use it

--- nvim/.config/nvim/after/plugin/luasnip.lua
+++ nvim/.config/nvim/after/plugin/luasnip.lua
@@ -1,11 +1,15 @@
 local ls = require("luasnip")
 local fmt = require("luasnip.extras.fmt").fmt
+local events = require "luasnip.util.events"
 local i = ls.insert_node
 local s = ls.s
 local t = ls.text_node
 local rep = require("luasnip.extras").rep
+local Region = require("refactoring.region")
 
--- TODO: Move luasnips to it's own file
+local Nodes = require("refactoring.treesitter.nodes")
+local InlineNode = Nodes.InlineNode
+
 -- Settings from teej
 local types = require("luasnip.util.types")
 ls.config.set_config({
@@ -40,14 +44,99 @@ ls.add_snippets("terraform", {
     ls.parser.parse_snippet("md", 'module "$1" {\n\tsource = "$0"\n}'),
 })
 
+--- Get whether or not import is present.
+---@param bufnr number: flag to require parameter types for codegen
+---@param filetype string: the filetype
+---@param import_text string: table of InlineNodes to query against
+---@param inline_nodes table: table of InlineNodes to query against
+---@return boolean, table
+local function check_for_import(bufnr, filetype, import_text, inline_nodes)
+    local tsparser = vim.treesitter.get_parser(bufnr, filetype)
+    local tstree = tsparser:parse()[1]
+    local out = {}
+
+    for _, statement in ipairs(inline_nodes) do
+        local temp = statement(tstree:root(), bufnr, filetype)
+        for _, node in ipairs(temp) do
+            table.insert(out, node)
+        end
+    end
+
+    local node_text
+    local found_import = false
+    for _, item in ipairs(out) do
+        node_text = vim.treesitter.query.get_node_text(item, bufnr)
+        if node_text == import_text then
+            found_import = true
+            break
+        end
+    end
+
+    return found_import, out
+end
+
+local function add_fmt_import_if_not_found_golang()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local inline_nodes = {
+        InlineNode("(import_spec path: (interpreted_string_literal) @capture)")
+    }
+    local import_text = "\"fmt\""
+
+    local found_import, out = check_for_import(
+        bufnr,
+        "go",
+        import_text,
+        inline_nodes)
+
+    if not found_import then
+        local node = out[1]
+        local node_text = vim.treesitter.query.get_node_text(node, bufnr)
+        local region = Region:from_node(node, bufnr)
+        local lsp_text_edit = region:to_lsp_text_edit(
+            import_text .. "\n\t" .. node_text)
+        vim.lsp.util.apply_text_edits({ lsp_text_edit }, bufnr, "utf-16")
+    end
+end
+
 ls.add_snippets("go", {
     s("ife", {
         t({ "if err != nil {", "\treturn " }),
         i(0),
         t({ "", "}" }),
     }),
-    s("prn", fmt('fmt.Println(fmt.Sprintf("{}: %v", {}))', { i(1), rep(1) })),
-    s("hh", { t({ 'fmt.Println("Hitting here!")' }) }),
+    s(
+        "prn",
+        fmt(
+            'fmt.Println(fmt.Sprintf("{}: %v", {}))',
+            { i(1), rep(1) }
+        ),
+        {
+            callbacks = {
+              [-1] = {
+                -- TODO: Can I make this more direct?
+                [events.enter] = function(_)
+                  add_fmt_import_if_not_found_golang()
+                end,
+              },
+            },
+        }
+    ),
+    s(
+        "hh",
+        {
+            t({ 'fmt.Println("Hitting here!")' })
+        },
+        {
+            callbacks = {
+              [-1] = {
+                -- TODO: Can I make this more direct?
+                [events.leave] = function(_)
+                  add_fmt_import_if_not_found_golang()
+                end,
+              },
+            },
+        }
+    ),
 })
 
 ls.add_snippets("python", {

40662980e73c45af32a6b7a5649001a7cd7f2e09
teddylear <teddylear@gmail.com>
2023-05-16 21:46:29 -0400

    feat(nvim): add harpoon component to lualine

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -1,9 +1,28 @@
+-- from below link
+-- https://github.com/dmmulroy/dotfiles/blob/main/.config/nvim/after/plugin/lualine.lua#L17-L41
+local function harpoon_component()
+	local mark_idx = require("harpoon.mark").get_current_index()
+	if mark_idx == nil then
+		return ""
+	end
+
+	return string.format("HP: %d", mark_idx)
+end
+
 require("lualine").setup({
     options = {
-        theme = "nord",
+        theme = "catppuccin",
         icons_enabled = true,
         -- This sets relative path
         globalstatus = true,
+    },
+    sections = {
+        lualine_b = {
+            "branch",
+            { harpoon_component },
+            "diff",
+            { "diagnostics", icons_enabled = false },
+        },
     },
     extensions = { "fzf", "fugitive" },
 })
